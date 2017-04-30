bc3952791e0b6c88743b5136aa02c844fa02b731
Will YE <iamthenewlebron@gmail.com>
2017-04-30 14:31:15 -0400

    dfsfdfasdfs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
-
+import java.util.*;
+import java.io.*;
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader("Scores"));
+        ArrayList<String> words = new ArrayList<>();
+        String lineJustFetched = null;
+        String[] wordsArray;
+        br.readLine();
+        while(true){
+            lineJustFetched = br.readLine();
+            if(lineJustFetched == null){
+                break;
+            }else{
+                wordsArray = lineJustFetched.split("\t");
+                for(String each : wordsArray){
+                    if(!"".equals(each)){
+                        words.add(each);
+                    }
+                }
+            }
+        }
+
+        for(String each : words){
+            System.out.println(each);
+        }
+
+        br.close();
     }
 }
